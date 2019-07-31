266880a3cca0cd2f524e75b23e0b705fd12e1754
Rhys Bray <rhysabray@hotmail.com>
2019-07-31 16:00:31 +0100

    First commit to keep project backed up. Currently has a controllable character (white circle) and enemies (red circles) that spawn at a slowly increased rate and chase the player

---
+++ top down shooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform[] spawnPoints;
+    public float spawnTime = 2f;
+    public float difficultyMultiplier = 0.99f;
+
+    private float spawnTimer;
+
+    private void Start()
+    {
+        spawnTimer = spawnTime;
+    }
+
+    private void Update()
+    {
+        if (spawnTimer <= 0)
+        {
+            int randomPositon = Random.Range(0, spawnPoints.Length);
+            Instantiate(enemy, spawnPoints[randomPositon].position, Quaternion.identity);
+            spawnTime *= difficultyMultiplier;
+            spawnTimer = spawnTime;
+        }
+        else
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+    }
+}
---
+++ top down shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+    private Vector2 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        velocity = movementDirection.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+    }
+}
