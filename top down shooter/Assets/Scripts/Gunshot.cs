4871591cf5fa6230aa69eccb39a609bea89e271d
Rhys Bray <rhysabray@hotmail.com>
2019-07-31 17:53:10 +0100

    player can now fire a projectile toward the cursor location. projectile is destroyed upon reaching the target location

---
+++ top down shooter/Assets/Scripts/Gunshot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunshot : MonoBehaviour
+{
+    public GameObject gunshot;
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(gunshot, playerTransform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ top down shooter/Assets/Scripts/GunshotMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunshotMovement : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Vector2 target;
+
+    private void Start()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target) < 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
